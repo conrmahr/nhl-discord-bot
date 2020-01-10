77a5742264e6a358452c15c8ee1badf878742335
conrmahr <conor@hireconor.com>
2020-01-10 04:47:17 -0600

    feat(Flags): eslint fix

--- commands/nhl.js
+++ commands/nhl.js
@@ -6,7 +6,7 @@ const querystring = require('querystring');
 module.exports = {
 	name: 'nhl',
 	usage: '<date> <team> <opponent> -<flag>',
-	description: 'Get games for \`today\`, \`tomorrow\`, \`yesterday\`, \`next\` 5 games, \`last\` 5 games, or a given date \`YYYY-MM-DD\`. If nothing is specified, games scheduled for today will return. Add abbreviations to filter for a specific team and opponent. Add flags \`-tv\` and/or \`-venue\`for more detail.',
+	description: 'Get games for `today`, `tomorrow`, `yesterday`, `next` 5 games, `last` 5 games, or a given date `YYYY-MM-DD`. If nothing is specified, games scheduled for today will return. Add abbreviations to filter for a specific team and opponent. Add flags `-tv` and/or `-venue`for more detail.',
 	category: 'scores',
 	aliases: ['nhl', 'n'],
 	examples: ['', 'nyi', 'tomorrow -tv -venue', 'next nyi nyr'],
