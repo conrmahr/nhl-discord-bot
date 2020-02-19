387b3a1532a9bdfcc470c808056b068854643227
conrmahr <conor@hireconor.com>
2020-02-18 21:42:05 -0600

    fix(UI): use personal imgur repo for NHL logo

--- commands/nhl.js
+++ commands/nhl.js
@@ -177,7 +177,7 @@ module.exports = {
 
 		const embed = new RichEmbed();
 		embed.setColor(0x59acef);
-		embed.setAuthor('NHL Scores', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/05_NHL_Shield.svg/150px-05_NHL_Shield.svg.png');
+		embed.setAuthor('NHL Scores', 'https://i.imgur.com/zQkDJU1.png');
 		schedule.dates.slice(0, limit).map(({ date, games }) => embed.addField(':hockey: ' + moment(date).format('ddd, MMM DD'), `${getScores(games)}`));
 
 
