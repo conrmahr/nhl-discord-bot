ca124dc32b73f2d1c597f76d3d8b8c738199f0a6
conrmahr <conor@hireconor.com>
2020-07-03 14:30:24 -0500

    feat(Command): eslint fix

--- commands/draft.js
+++ commands/draft.js
@@ -15,35 +15,33 @@ module.exports = {
 
 		const endpoint = 'https://statsapi.web.nhl.com/api/v1/draft/';
 		const parameters = {};
-		let year = moment().format('YYYY');
+		const draftRound = '1';
+		let draftYear = moment().format('YYYY');
 		let current = moment().format('YYYY');
-		let round = '1';
 		let draftObj = '';
-		let tableObj = '';
-		let standingsObj = '';
 		let draftLogo = 'https://i.imgur.com/zl8JzZc.png';
-		let draftTitle = 'NHL Draft';
+		const draftTitle = 'NHL Draft';
 		let draftTeam = '';
 
 		if (moment(args[0], 'YYYY', true).isValid()) {
 			const nextSeason = parseInt(args[0], 10) + 1;
 			current = `${args[0]}${nextSeason}`;
-			year = args[0];
+			draftYear = args[0];
 			args.push('0');
 			if (!args[0]) return message.reply(`${args[0]} is not a valid draft year. Type \`${prefix}help draft\` for a list of arguments.`);
 		}
 		else {
 			args.push(args[0]);
 		}
-		const data = await fetch(`https://statsapi.web.nhl.com/api/v1/draft/${year}`).then(response => response.json());
+		const data = await fetch(`${endpoint}${draftYear}`).then(response => response.json());
 
 		if (!data.drafts[0].rounds) return message.reply(`no draft picks found. Type \`${prefix}help draft\` for a list of arguments.`);
 		parameters.season = current;
-		let query = qs.stringify(parameters, { addQueryPrefix: true });
+		const query = qs.stringify(parameters, { addQueryPrefix: true });
 
 		const { teams } = await fetch(`https://statsapi.web.nhl.com/api/v1/teams/${query}`).then(response => response.json());
-		const flatFilterPicks = (data, pred) => data.drafts.flatMap(({rounds}) => {
-		  return rounds.flatMap(({picks}) => picks.filter(pred));
+		const flatFilterPicks = (draft, pred) => draft.drafts.flatMap(({ rounds }) => {
+			return rounds.flatMap(({ picks }) => picks.filter(pred));
 		});
 
 		if (args[1]) {
@@ -62,34 +60,33 @@ module.exports = {
 					draftObj = flatFilterPicks(data, pick => pick.team.id === teamObj.id);
 				}
 				else {
-					return message.reply(`\`${args[1]}\` is not a team for the ${year} Draft. Type \`${prefix}help draft\` for a list of arguments.`);
+					return message.reply(`\`${args[1]}\` is not a team for the ${draftYear} Draft. Type \`${prefix}help draft\` for a list of arguments.`);
 				}
 			}
 			else if (parseInt(args[1], 10) > 0) {
 				draftTeam = ` (Round ${args[1]})`;
 				draftObj = flatFilterPicks(data, pick => pick.round === args[1]);
 
-				if (!draftObj.length > 0) return message.reply(`\`${args[1]}\` is not a valid round for the ${year} Draft. Type \`${prefix}help draft\` for a list of arguments.`);
+				if (!draftObj.length > 0) return message.reply(`\`${args[1]}\` is not a valid round for the ${draftYear} Draft. Type \`${prefix}help draft\` for a list of arguments.`);
 			}
 			else {
-				draftTeam = ` (Round ${round})`;
-				draftObj = flatFilterPicks(data, pick => pick.round === round);
-				if (!draftObj) return message.reply(`no draft picks found for the ${year} Draft. Type \`${prefix}help draft\` for a list of arguments.`);
+				draftTeam = ` (Round ${draftRound})`;
+				draftObj = flatFilterPicks(data, pick => pick.round === draftRound);
+				if (!draftObj) return message.reply(`no draft picks found for the ${draftYear} Draft. Type \`${prefix}help draft\` for a list of arguments.`);
 			}
 
 		}
 
 		draftObj.sort(function(a, b) {
-				return a['pickOverall'] - b['pickOverall'];
+			return a['pickOverall'] - b['pickOverall'];
 		});
 
 		function getPicks(rounds) {
 			let r = 0;
 			return rounds.map(pick => {
 				r++;
-				const { year, round, pickOverall, pickInRound, team, prospect } = pick
+				const { round, pickOverall, team, prospect } = pick;
 				const teamAbbreviation = teams.find(o => o.id === team.id).abbreviation;
-			
 				function getHeader() {
 					if (r === 1) return '#Rd Pick Team Player\n';
 					return '';
@@ -111,7 +108,7 @@ module.exports = {
 		}
 
 		const block = '```md\n' + getPicks(draftObj) + '```';
-		const authorArr = [year, draftTitle, draftTeam];
+		const authorArr = [draftYear, draftTitle, draftTeam];
 		const embed = new RichEmbed();
 		embed.setColor(0x59acef);
 		embed.setAuthor(authorArr.join(' '), 'https://i.imgur.com/zl8JzZc.png');
