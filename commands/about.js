b859fcfc1959c445ceb404632703972f26386b20
conrmahr <conor@hireconor.com>
2020-08-01 11:15:44 -0500

    chore(Embed): cleanup about text

--- commands/about.js
+++ commands/about.js
@@ -14,9 +14,9 @@ module.exports = {
 		embed.setColor(0x59acef);
 		embed.setAuthor('About', 'https://cdn.discordapp.com/avatars/535203406592344067/1473d566732ea6ffd24d02be45af8b21.png');
 		embed.setDescription([
-			`**${name}** is developed by **[@conrmahr](https://github.com/conrmahr)** and uses the **[Discord.js](https://discord.js.org)** library.`,
+			`**${name}** is developed by **[@conrmahr](https://github.com/conrmahr)** using the **[Discord.js](https://discord.js.org)** library.`,
 
-			`Issues or feature requests can be submitted to the **[GitHub repo](https://github.com/conrmahr/${name})**.`,
+			`Issues and/or feature requests can be submitted through **[GitHub](https://github.com/conrmahr/${name})**.`,
 			'',
 			`${description} v${version}.`,
 		]);
