de8e84241a4837f7145afb2cd56484f05415a91c
conrmahr <conor@hireconor.com>
2021-05-13 17:03:21 -0500

    feat(Embed): adding gameType to nhl and game commands

--- commands/game.js
+++ commands/game.js
@@ -58,7 +58,7 @@ module.exports = {
 
 		if (!parameters.teamId) return message.reply(`no team was defined. Type \`${prefix}help game\` for a list of arguments.`);
 
-		parameters.expand = ['schedule.teams', 'schedule.linescore'];
+		parameters.expand = ['schedule.teams', 'schedule.linescore', 'schedule.game.seriesSummary'];
 		let flagLineup = false;
 		let flagScoring = false;
 		let flagPenalty = false;
@@ -103,10 +103,11 @@ module.exports = {
 					return `${remain}${spacer}${ordinal}`;
 				}
 
-				const { link, status: { statusCode }, gameDate, teams: { away, home }, linescore, venue, content } = game;
+				const { link, gameType, status: { statusCode }, gameDate, teams: { away, home }, linescore, venue, content, seriesSummary } = game;
 				const gameObj = {
 					author: '',
 					authorURL: 'https://i.imgur.com/zl8JzZc.png',
+					type: gameType,
 					status: Number(statusCode),
 					date: gameDate,
 					awayTeam: away.team.abbreviation,
@@ -135,7 +136,18 @@ module.exports = {
 					live: link,
 					content: content.link,
 					venue: venue.name,
+					match: '',
 				};
+
+				if (gameObj.type === 'PR') {
+					gameObj.match = ' *Pre-season\n';
+				}
+				else if (gameObj.type === 'A') {
+					gameObj.match = ` *All-Star game\n`;
+				}
+				else if (gameObj.type === 'P' && seriesSummary) {
+					gameObj.match = ` *${seriesSummary.seriesStatus}\n`;
+				}
 
 				if (gameObj.status < 3) {
 					const gameTimeEST = moment(game.gameDate).tz('America/New_York').format('h:mm A z');
@@ -219,6 +231,7 @@ module.exports = {
 				scoreboardStr += `├${''.padEnd(b[1], '─')}${''.padEnd(20 + o, '────')}─┤\n`;
 				scoreboardStr += `│ ${gameObj.homeTeamLine}  │\n`;
 				scoreboardStr += `└${''.padEnd(b[0] + o, '─')}┘\n`;
+				scoreboardStr += gameObj.match;
 				scoreboardStr += '```';
 				gameObj.scoreboard = scoreboardStr;
 
--- commands/nhl.js
+++ commands/nhl.js
@@ -79,7 +79,7 @@ module.exports = {
 			}
 		}
 
-		parameters.expand = ['schedule.teams', 'schedule.linescore'];
+		parameters.expand = ['schedule.teams', 'schedule.linescore', 'schedule.game.seriesSummary'];
 		let flagVenue = false;
 		let flagHide = false;
 		for (const flag of flags) {
@@ -123,13 +123,22 @@ module.exports = {
 					return `(${remain}${spacer}${ordinal})`;
 				}
 
-				const { status: { statusCode }, teams: { away, home }, linescore, broadcasts, venue } = game;
+				const { gameType, status: { statusCode }, teams: { away, home }, linescore, broadcasts, venue, seriesSummary } = game;
 				const awayTeam = away.team.abbreviation;
 				const homeTeam = home.team.abbreviation;
 				const awayBB = isBold(away.score, home.score);
 				const homeBB = isBold(home.score, away.score);
+				let match = '';
 				let tv = '';
 				let arena = '';
+
+				if ( gameType === 'P' && seriesSummary) {
+					match = `[${seriesSummary.seriesStatusShort}] `;
+				}
+				else if ( gameType !== 'R') {
+					match = `[${gameType}] `;
+				}
+
 				if (broadcasts) {
 					const channels = broadcasts.map(i => i.name).join(', ');
 					tv = ` :tv: [${channels}] `;
@@ -142,7 +151,7 @@ module.exports = {
 				if (statusCode < 3 || flagHide) {
 					const gameTimeEST = moment(game.gameDate).tz('America/New_York').format('h:mm A z');
 					const gameTime = (statusCode > 2) ? formatPeriod(linescore.currentPeriodTimeRemaining, linescore.currentPeriodOrdinal) : gameTimeEST;
-					return `${awayTeam} @ ${homeTeam} ${gameTime}${arena}${tv}`;
+					return `${match}${awayTeam} @ ${homeTeam} ${gameTime}${arena}${tv}`;
 				}
 				else if (statusCode > 2 && statusCode < 5) {
 					const clock = function getClock(timeLeft, type) {
@@ -158,7 +167,7 @@ module.exports = {
 					return `${awayTeam} ${away.score} ${awayPP}${awayEN} ${homeTeam} ${home.score} ${homePP}${homeEN} ${formatPeriod(linescore.currentPeriodTimeRemaining, linescore.currentPeriodOrdinal)}${intermission}${arena}${tv}`;
 				}
 				else if (statusCode > 4 && statusCode < 8) {
-					return `${awayBB}${awayTeam} ${away.score}${awayBB} ${homeBB}${homeTeam} ${home.score}${homeBB} ${formatPeriod(linescore.currentPeriodTimeRemaining, linescore.currentPeriodOrdinal)}${arena}`;
+					return `${match}${awayBB}${awayTeam} ${away.score}${awayBB} ${homeBB}${homeTeam} ${home.score}${homeBB} ${formatPeriod(linescore.currentPeriodTimeRemaining, linescore.currentPeriodOrdinal)}${arena}`;
 				}
 				else if (statusCode === '8') {
 					return `${awayTeam} @ ${homeTeam} TBD`;
